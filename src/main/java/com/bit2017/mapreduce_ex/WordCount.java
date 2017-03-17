ea96d18fc1489dc98d3ae846273183820dbb1c92
yukio0oa <yuki_o0oa@naver.com>
2017-03-17 15:00:57 +0900

    hadoop mapreduce

---
+++ src/main/java/com/bit2017/mapreduce_ex/WordCount.java
@@ -0,0 +1,31 @@
+package com.bit2017.mapreduce_ex;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCount {
+
+	public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+		
+	}
+	
+	public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = new Job(conf, "WordCount");
+		
+		// 1. job instance 초기화 작업
+		// job.setJarByClass(WordCount.class);
+		
+		// 실행
+		job.waitForCompletion(true);
+	}
+
+}
