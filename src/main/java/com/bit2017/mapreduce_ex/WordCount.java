eb3e9e590215dce82fb0c0ccdf1d6c738256baef
yukio0oa <yuki_o0oa@naver.com>
2017-03-20 14:29:54 +0900

    log, override clean

--- src/main/java/com/bit2017/mapreduce_ex/WordCount.java
+++ src/main/java/com/bit2017/mapreduce_ex/WordCount.java
@@ -15,12 +15,23 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
+import com.sun.org.apache.commons.logging.Log;
+import com.sun.org.apache.commons.logging.LogFactory;
+
 public class WordCount {
+	private static Log log = LogFactory.getLog(WordCount.class);
 	
 	public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
 		private Text word = new Text();
 		private static LongWritable one = new LongWritable(1);
 		
+		
+		@Override
+		protected void setup(Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			log.info("--------> setup() called");
+		}
+
 		@Override
 		protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, LongWritable>.Context context)
 				throws IOException, InterruptedException {
@@ -32,7 +43,21 @@ public class WordCount {
 			}
 		}
 		
-	}
+		
+		@Override
+		protected void cleanup(Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			log.info("--------> cleanup() called");
+		}
+		
+		//run은 보통 Override하지 않는다.
+		/*@Override
+		public void run(Mapper<LongWritable, Text, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			log.info("--------> run() called");
+		}
+		*/		
+	}	
 	
 	public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable> {
 		
