24aa44cb2c3eed5881faeb2c34e00b5d919a01e9
yukio0oa <yuki_o0oa@naver.com>
2017-03-21 16:14:36 +0900

    하하

--- src/main/java/com/bit2017/mapreduce_ex/WordCount2.java
+++ src/main/java/com/bit2017/mapreduce_ex/WordCount2.java
@@ -5,7 +5,6 @@ import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
